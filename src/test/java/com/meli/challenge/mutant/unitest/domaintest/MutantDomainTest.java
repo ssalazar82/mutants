ba70c109fb05b9f5253e26185a9cfce8e27786d5
Sergio Salazar <sergiossl@hotmail.com>
2021-09-27 00:02:50 -0500

    Test
    unit test add

--- src/test/java/com/meli/challenge/mutant/unitest/domaintest/MutantDomainTest.java
+++ src/test/java/com/meli/challenge/mutant/unitest/domaintest/MutantDomainTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,8 @@ import com.meli.challenge.mutant.domain.exception.BadDnaSequenceException;
 import com.meli.challenge.mutant.domain.service.MutantService;
 import com.meli.challenge.mutant.domain.service.contract.IMatrixService;
 import com.meli.challenge.mutant.domain.service.contract.IMutantService;
+import com.meli.challenge.mutant.infrastructure.dto.StatDto;
+import com.meli.challenge.mutant.infrastructure.service.DnaService;
 import com.meli.challenge.mutant.infrastructure.service.contract.IDnaService;
 import com.meli.challenge.mutant.unitest.builder.MutantDnaBuilder;
 import com.meli.challenge.mutant.unitest.builder.MutantDnaIncorrectBuilder;
@@ -51,6 +54,60 @@ public class MutantDomainTest {
 		});
 			
 		assertThat(thrown).isInstanceOf(BadDnaSequenceException.class);
+
+	}
+	
+	/*
+	 * Valida calculo de ratio en las estadisticas
+	 * */
+	@Test
+	public void DtoStatsTest() {
+		StatDto stats = new StatDto();
+		DnaService dnaService = mock(DnaService.class);
+		when(dnaService.countHumans()).thenReturn(100);
+		when(dnaService.countMutants()).thenReturn(40);
+		
+		stats.setCountHumanDna(dnaService.countHumans());
+		stats.setCountMutantDna(dnaService.countMutants());
+		stats.setRatio(0);
+		
+		assertThat(stats.getRatio()).isEqualTo(0.4);
+
+	}
+
+	/*
+	 * Valida get de las estadisticas - Human
+	 * */
+	@Test
+	public void DtoStatsGetHumanTest() {
+		StatDto stats = new StatDto();
+		DnaService dnaService = mock(DnaService.class);
+		when(dnaService.countHumans()).thenReturn(100);
+		when(dnaService.countMutants()).thenReturn(40);
+		
+		stats.setCountHumanDna(dnaService.countHumans());
+		stats.setCountMutantDna(dnaService.countMutants());
+		stats.setRatio(0);
+		
+		assertThat(stats.getCountHumanDna()).isEqualTo(100);
+
+	}
+	
+	/*
+	 * Valida get de las estadisticas - Mutant
+	 * */
+	@Test
+	public void DtoStatsGetMutantTest() {
+		StatDto stats = new StatDto();
+		DnaService dnaService = mock(DnaService.class);
+		when(dnaService.countHumans()).thenReturn(100);
+		when(dnaService.countMutants()).thenReturn(40);
+		
+		stats.setCountHumanDna(dnaService.countHumans());
+		stats.setCountMutantDna(dnaService.countMutants());
+		stats.setRatio(0);
+		
+		assertThat(stats.getCountMutantDna()).isEqualTo(40);
 
 	}
 }
